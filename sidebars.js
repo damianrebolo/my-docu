75e4240d1eacee48bfdf736f4e8e8c6acac05be2
Damian Rebolo <damianrebolo@gmail.com>
2021-01-04 17:15:13 +0100

    add reset css document

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     Authentication: ['secure-auth'],
+    CSS: ['reset-css'],
     // JavaScript: ['secure-auth'],
     // AWS: ['secure-auth'],
     Features: ['mdx', 'doc1'],
