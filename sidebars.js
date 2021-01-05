17e599f750054e37608780d80c5d2423d77b514f
Damian Rebolo <damianrebolo@gmail.com>
2021-01-05 16:57:56 +0100

    more documentation added

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,13 @@
 module.exports = {
   someSidebar: {
-    General: ['doc1', 'reset-css', 'secure-auth', 'ioc']
+    General: [
+      'doc1',
+      'reset-css',
+      'secure-auth',
+      'ioc',
+      'destructuring-assignment',
+      'spread-syntax'
+    ]
     // 'Programming principles': ['ioc']
     // CSS: ['reset-css'],
     // Authentication: ['secure-auth']
