61ef85806e9279fd4285146ed46ab05d3c6f8857
Damian Rebolo <damianrebolo@gmail.com>
2022-09-11 10:55:38 +0700

    feat: home page features removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,9 +50,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      {/* <main><HomepageFeatures /></main> */}
     </Layout>
   );
 }
